ba61c9b7606de56949832d2d805c19efb9900f2b
Suzie Wong <suzie@ubuntu.(none)>
2013-04-21 21:19:47 +0800

    fir

---
+++ README.md
@@ -0,0 +1,1 @@
+/usr/local/bin/gunicorn -b 0.0.0.0:10000 code -D
---
+++ code.py
@@ -0,0 +1,17 @@
+import web
+
+urls = (
+    '/', 'index'
+)
+
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
+app = web.application(urls, globals(), autoreload=False)
+application = app.wsgifunc()
+
